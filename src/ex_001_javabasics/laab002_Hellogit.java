8f23c5f8443a69d57e9204f78a5d9ad8e191b7c9
Neha24 <nehacris@gmail.com>
2025-12-11 21:09:08 +0530

    all clear

--- src/ex_001_javabasics/laab002_Hellogit.java
+++ src/ex_001_javabasics/laab002_Hellogit.java
@@ -2,5 +2,5 @@ package ex_001_javabasics;
 
 public class laab002_Hellogit {
     public static void main(String[] args)
-    {System.out.println(" Hello git ");}
+    {System.out.println("Hello git");}
 }
