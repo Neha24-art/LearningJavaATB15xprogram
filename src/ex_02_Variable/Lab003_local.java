0d548b618fa250421c9809bd219e5efd08b19e77
amitr <nehacris@gmai.com>
2025-12-22 06:34:07 +0530

    localvariable

---
+++ src/ex_02_Variable/Lab003_local.java
@@ -0,0 +1,10 @@
+package ex_02_Variable;
+
+public class Lab003_local {
+    static void main() {
+        int i=10;
+       System.out.println("the no. is="+i);
+
+    }
+    System.out.println("the no. is="+i);
+}
